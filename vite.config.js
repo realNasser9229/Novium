d006a32795a8924962117a71d8d1277335afc6d8
Nas9229alt <nassysussy@gmail.com>
2026-03-09 21:25:43 +0300

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // CHANGE THIS ↓↓↓ to your repo name (or '/' if it's your username.github.io repo)
 export default defineConfig({
   plugins: [react()],
-  base: '/Novium/'
+  base: '/RazHouse/'
 })
